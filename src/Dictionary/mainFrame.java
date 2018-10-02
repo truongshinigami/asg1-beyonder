241fc9e54ea3716b722b83ee942999832952ac8e
Hà Văn Bắc <havanbac99@gmail.om>
2018-10-02 23:55:01 +0700

    test

---
+++ src/Dictionary/mainFrame.java
@@ -0,0 +1,21 @@
+package dictionary;
+
+import javax.swing.*;
+
+public class mainFrame {
+    public static  void main(String[] a){
+        JFrame layout = new JFrame();
+        layout.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        layout.setVisible(true);
+        layout.setSize(500, 500);
+
+    }
+
+
+
+
+
+
+
+}
+
--- src/hello2.java
+++
@@ -1,5 +0,0 @@
-public class hello2 {
-    public static void main(String[] a){
-        System.out.println("dhghjgf0hgh");
-    }
-}
